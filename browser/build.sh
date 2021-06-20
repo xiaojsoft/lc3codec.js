b496c00c97d4fc66a4758d5921e0aea9cb36e8c2
Suwa Mitsuo <suwa-mitsuo@outlook.com>
2021-06-21 02:43:20 +0800

    Added build script for browser.
    Change(s):
      [1] Added build script for browser.
    
    Contributor(s):
      [1] Suwa Mitsuo <suwa-mitsuo@outlook.com>

---
+++ browser/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+#  Copyright 2021 XiaoJSoft Studio. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE.md file.
+#
+
+#  Go to the script directory.
+SCRIPTDIR="`realpath \"$0\"`"
+cd "`dirname \"${SCRIPTDIR}\"`"
+
+#  Generate bundle.
+node bundle.js
+if [ "$?" != "0" ]; then
+    exit 1
+fi
+npx javascript-obfuscator "dist/lc3.js" --compact true --identifier-names-generator mangled --string-array false --rename-globals true --output "dist/lc3.min.js"
+if [ "$?" != "0" ]; then
+    exit 1
+fi
+
+#  Generate ES5-compatible script.
+npx babel "dist/lc3.js" --out-file "dist/lc3.es5.js"
+if [ "$?" != "0" ]; then
+    exit 1
+fi
+npx javascript-obfuscator "dist/lc3.es5.js" --compact true --identifier-names-generator mangled --string-array false --rename-globals true --output "dist/lc3.es5.min.js"
+if [ "$?" != "0" ]; then
+    exit 1
+fi
+
+exit $?
---
+++ browser/bundle.js
@@ -0,0 +1,73 @@
+//
+//  Copyright 2021 XiaoJSoft Studio. All rights reserved.
+//  Use of this source code is governed by a BSD-style license that can be
+//  found in the LICENSE.md file.
+//
+
+//
+//  Imports.
+//
+
+//  Imported modules.
+const FS = require("fs");
+const Path = require("path");
+
+//
+//  Constants.
+//
+
+//  Build configurations.
+const CONFIG_GLOBAL = "window";
+const CONFIG_NAMESPACE = "LC3";
+const CONFIG_OUTFILE = Path.join(__dirname, "dist", "lc3.js");
+
+//
+//  Main.
+//
+(function() {
+    //  Read template.
+    let template = FS.readFileSync(Path.join(__dirname, "template.txt"), {
+        "encoding": "utf-8"
+    });
+
+    //  Read module list.
+    let module_list = JSON.parse(FS.readFileSync(
+        Path.join(__dirname, "modules.json"), 
+        {
+            "encoding": "utf-8"
+        }
+    ));
+
+    //  Generate script.
+    let module_root = Path.join(__dirname, "..");
+    let module_generated = "{\n";
+    for (let i = 0; i < module_list.length; ++i) {
+        let module_name = module_list[i];
+        let module_content = FS.readFileSync(
+            Path.join(module_root, module_name + ".js"),
+            {
+                "encoding": "utf-8"
+            }
+        );
+        module_generated += "    \"" + module_name + "\": function(module, require) {\n";
+        module_generated += module_content;
+        if (!module_generated.endsWith("\n")) {
+            module_generated += "\n";
+        }
+        if (i + 1 != module_list.length) {
+            module_generated += "    },\n";
+        } else {
+            module_generated += "    }\n";
+        }
+    }
+    module_generated += "}";
+    let output = template;
+    output = output.replace("${BUILD_GLOBAL}", CONFIG_GLOBAL);
+    output = output.replace("${BUILD_NAMESPACE}", CONFIG_NAMESPACE);
+    output = output.replace("${BUILD_MODULE_DATA}", module_generated);
+    
+    //  Write output file.
+    FS.writeFileSync(CONFIG_OUTFILE, output, {
+        "encoding": "utf-8"
+    });
+})();
